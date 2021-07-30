a308514892c92dcd9b1bb3c2e392da784eb45e21
spjustice <sanyok.vergun@gmail.com>
2021-07-30 11:00:19 +0300

    complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,28 @@
 //select open-close
+// $('.select-header').on('click', function () {
+//    var btn = $(this);
+//    var block = btn.closest('.select');
+//    block.find('.select-body').slideToggle();
+//    block.find('.select-icon').toggleClass('icon-triangle-right');
+//    block.find('.select-icon').toggleClass('icon-triangle-down');
+// })
 $('.select-header').on('click', function () {
    var btn = $(this);
    var block = btn.closest('.select');
+   var bodyBlock = block.closest('.details__container');
+   var blocks = bodyBlock.find('.select');
+   if (!block.hasClass('open')) {
+
+      $(blocks).each(function () {
+         if ($(this).hasClass('open')) {
+            $(this).find('.select-body').slideToggle();
+            $(this).removeClass('open');
+            $(this).find('.select-icon').toggleClass('icon-triangle-right');
+            $(this).find('.select-icon').toggleClass('icon-triangle-down');
+         }
+      })
+   }
+   block.toggleClass('open');
    block.find('.select-body').slideToggle();
    block.find('.select-icon').toggleClass('icon-triangle-right');
    block.find('.select-icon').toggleClass('icon-triangle-down');
